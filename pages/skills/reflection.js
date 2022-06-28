dae18132dab67177918f9a49d7a29271b9735778
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2022-06-28 00:05:45 -0600

    fixed error in reflection.js

--- pages/skills/reflection.js
+++ pages/skills/reflection.js
@@ -12,7 +12,7 @@ export default function Reflection() {
         <button className='home-btn'><Link href='/'>Home</Link></button>
             <h4>Reflection on Past Experience:</h4>
             <ol type='a'>
-                <li>I'm looking for a postition where I can have the opportunity
+                <li>I am looking for a postition where I can have the opportunity
                     to use my communication and technical skills. As a customer service 
                     representative and technical support speacialist, I would be
                      able to apply my years of experience by engaging with customers
